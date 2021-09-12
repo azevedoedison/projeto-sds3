53eee3cf4e25ad85e9f437a0906ce03324508c94
azevedoedison <azevedoedison@gmail.com>
2021-09-11 23:23:22 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,7 +1,62 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
+
+
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+}
 
 const BarChart = () => {
     
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: {
+            categories: []
+        },
+        series: [
+            {
+                name: "",
+                data: []
+            }
+        ]
+    });
+
+    /** UserEffect: Hook que serve para executar algo na instanciação ou na destruição do componente. 
+   * Serve para observar estado também. */  
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/sucess-by-seller`).then(response => {
+        
+        const data = response.data as SaleSuccess[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1 ));
+
+        setChartData({
+            labels: {
+                categories: myLabels
+            },
+            series: [
+                {
+                    name: "Success",
+                    data: mySeries
+                }
+            ]
+        });
+        //console.log(chartData);
+    });
+  } , []);
+
+
     const options = {
         plotOptions: {
             bar: {
@@ -10,7 +65,7 @@ const BarChart = () => {
         },
     };
 
-    const mockData = {
+    /*const mockData = {
         labels: {
             categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
         },
@@ -20,12 +75,12 @@ const BarChart = () => {
                 data: [43.6, 67.1, 67.7, 45.6, 71.1]
             }
         ]
-    };
+    };*/
 
     return (
         <Chart
-            options={{...options, xaxis: mockData.labels}} 
-            series={mockData.series}
+            options={{...options, xaxis: chartData.labels}} 
+            series={chartData.series}
             type="bar"
             height="240"
         />
